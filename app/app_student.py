7c4885b8c69092ad1d5c20880b36d55c05ea4b5d
StudentTboi <thoa0046@student.monash.edu>
2024-10-05 16:00:55 +1000

    Update app_student.py
    adding instantiation for student class

--- app/app_student.py
+++ app/app_student.py
@@ -1,1 +1,16 @@
+"""
+FIT1056 EMPOWERU
+"""
+from app_lecturer import User
 
+class StudentUser(User):
+  def __init__(self, uid, first_name, last_name, date_of_birth, contact_name, contact_num):
+    """
+    Constructor
+    """
+    super().__init__(uid, first_name, last_name, contact_num)
+    self.date_of_birth = date_of_birth
+    self.contact_name = contact_name
+
+if __name__ == "__main__":
+  pass
