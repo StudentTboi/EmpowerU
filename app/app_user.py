40f7cd6067611c5c91d072f1238d8b31d472f6a2
maqi0001 <maqi0001@student.monash.edu>
2024-10-05 21:45:36 +1000

    Update app_user.py
    Added the class

--- app/app_user.py
+++ app/app_user.py
@@ -1,1 +1,14 @@
+class User:
 
+    def __init__(self, uid, first_name, last_name, contact_num, contact_email):
+        """
+        Constructor for the User class.
+        """
+        self.uid = uid
+        self.first_name = first_name
+        self.last_name = last_name
+        self.contact_num = contact_num
+        self.contact_email = contact_email
+
+if __name__ == "__main__":
+    pass
