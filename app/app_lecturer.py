06b7e87d54c4385053ae2b3d2593481e97da5f72
maqi0001 <maqi0001@student.monash.edu>
2024-10-13 01:52:07 +1100

    Update app_lecturer.py

--- app/app_lecturer.py
+++ app/app_lecturer.py
@@ -1,5 +1,6 @@
 import os
 from app.app_user import User
+from app.app_student import StudentUser
 
 class LecturerUser(User):
 
@@ -16,18 +17,23 @@ class LecturerUser(User):
         - an instance of LecturerUser corresponding to the username if successful,
           None otherwise
         """
-        recept_path = "./authenticate/lecturers.txt"
-        if os.path.exists(recept_path):
+        lecturer_path = "./authenticate/lecturers.txt"
+        if os.path.exists(lecturer_path):
             with open(lecturer_path, "r", encoding="utf8") as rf:
                 lines = rf.readlines()
+
             for line in lines:
                 # Sequence unpacking: 
                 # https://docs.python.org/3/tutorial/datastructures.html#tuples-and-sequences
+                if len(line)<5: #Checking if line is invalid
+                    print(f"WARNING: A line in {lecturer_path} is invalid")
+                    continue
+                
                 lecturer_id, first_name, last_name, contact_num, username, password = line.strip("\n").split(",")
                 
                 if input_username == username:
                     if input_password == password:
-                        return LecturerUser(recept_id, first_name, last_name, contact_num, input_username, input_password)
+                        return LecturerUser(lecturer_id, first_name, last_name, contact_num, input_username, input_password)
                     else:
                         return None # or return, or break
         else:
@@ -41,28 +47,26 @@ class LecturerUser(User):
         self.teaching_area = teaching_area
 
     def import_students_data(self):
-         """
-         Method to read students data and store it into the lecturer's session.
+        """
+        Method to read students data and store it into the lecturer's session.
 
-         Parameter(s):
-         (None)
+        Parameter(s):
+        (None)
 
-         Returns:
-         (None)
-         """
-         self.students = []
-         students_path = "./data/pst4_students.txt"
-         if os.path.exists(students_path):
-             with open(students_path, "r", encoding="utf8") as rf:
-                 lines = rf.readlines()
-             for line in lines:
-                 student_id, first_name, last_name, date_of_birth, contact_name, contact_num = line.strip("\n").split(",")
-                 student_obj = StudentUser(student_id, first_name, last_name, date_of_birth, contact_name, contact_num)
-                 self.students.append(student_obj)
-         else:
-             print(f"Please check the subdirectory and file exists for {students_path}.")
-
-
+        Returns:
+        (None)
+        """
+        self.students = []
+        students_path = "./data/pst4_students.txt"
+        if os.path.exists(students_path):
+            with open(students_path, "r", encoding="utf8") as rf:
+                lines = rf.readlines()
+                for line in lines:
+                    student_id, first_name, last_name, date_of_birth, contact_name, contact_num = line.strip("\n").split(",")
+                    student_obj = StudentUser(student_id, first_name, last_name, date_of_birth, contact_name, contact_num)
+                    self.students.append(student_obj)
+        else:
+            print(f"Please check the subdirectory and file exists for {students_path}.")
 
 if __name__ == "__main__":
     pass
