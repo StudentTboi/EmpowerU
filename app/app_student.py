164b29d3f3caeaaf7f4e3577f812a755fee53358
maqi0001 <maqi0001@student.monash.edu>
2024-10-13 01:52:31 +1100

    Update app_student.py

--- app/app_student.py
+++ app/app_student.py
@@ -1,16 +1,39 @@
 """
 FIT1056 EMPOWERU
 """
-from app_lecturer import User
+import os
+
+from app.app_lecturer import User
 
 class StudentUser(User):
-  def __init__(self, uid, first_name, last_name, date_of_birth, contact_name, contact_num):
-    """
-    Constructor
-    """
-    super().__init__(uid, first_name, last_name, contact_num)
-    self.date_of_birth = date_of_birth
-    self.contact_name = contact_name
+    @staticmethod
+    def authenticate(input_username, input_password):
+        student_path = "./authenticate/students.txt"
+        if os.path.exists(student_path):
+            with open(student_path, "r", encoding="utf8") as rf:
+                lines = rf.readlines()
+            for line in lines:
+                if len(line)<5: #Checking if line is invalid
+                    print(f"WARNING: A line in {student_path} is invalid")
+                    continue
+                
+                student_id, first_name, last_name, contact_num, username, password = line.strip("\n").split(",")
+                
+                if input_username == username:
+                    if input_password == password:
+                        return StudentUser(student_id, first_name, last_name, contact_num, input_username, input_password)
+                    else:
+                        return None # or return, or break
+                else:
+                    print(f"Please check subdirectory and file {student_path} exists.")
+
+    def __init__(self, uid, first_name, last_name, date_of_birth, contact_name, contact_num):
+        """
+        Constructor
+        """
+        super().__init__(uid, first_name, last_name, contact_num)
+        self.date_of_birth = date_of_birth
+        self.contact_name = contact_name
 
 if __name__ == "__main__":
   pass
