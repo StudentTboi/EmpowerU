d42ffdd068aa1e43222977f941074b8e8e68aca5
EllieC919 <102844395+EllieC919@users.noreply.github.com>
2024-10-18 14:54:37 +1100

    Create gui_course_materials.py

---
+++ interface/gui_course_materials.py
@@ -0,0 +1,15 @@
+"""
+EmpowerU
+
+This file contains the class definition for the CourseMaterials class.
+"""
+
+# Third party imports
+import tkinter as tk
+import app_utils as util
+
+class CourseMaterials(tk.Frame):
+    def __init__(self, master, user):
+      self.master=master
+      self.user-user
+        
