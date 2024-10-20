fac1996d394c6d629264c3c5c1e114a7d10bdb7c
maqi0001 <maqi0001@student.monash.edu>
2024-10-20 15:36:36 +1100

    Update app_user.py

--- app/app_user.py
+++ app/app_user.py
@@ -1,8 +1,17 @@
 class User:
-
     def __init__(self, uid, first_name, last_name, date_of_birth, contact_num, contact_email, username, password):
         """
         Constructor for the User class.
+        
+        Parameters:
+        - uid (str): Unique identifier for the user.
+        - first_name (str): The user's first name.
+        - last_name (str): The user's last name.
+        - date_of_birth (str): The user's date of birth.
+        - contact_num (str): The user's contact number.
+        - contact_email (str): The user's email address.
+        - username (str): The user's username for authentication.
+        - password (str): The user's password for authentication.
         """
         self.uid = uid
         self.first_name = first_name
