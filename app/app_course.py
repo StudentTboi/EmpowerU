5384e1244cdc4713a6ac081132c8f6e0aec95abe
maqi0001 <maqi0001@student.monash.edu>
2024-10-20 15:34:37 +1100

    Update app_course.py

--- app/app_course.py
+++ app/app_course.py
@@ -1,7 +1,23 @@
 from app.app_quiz import Quiz
 
 class Course:
+    """
+    Represents a course with specific learning materials and a quiz.
+
+    Attributes:
+    - course_type (str): The type of course (e.g., 'AI', 'InfoSec', 'PyLearn').
+    - filepath (str): The path to the course's learning materials.
+    - reading_materials (list): List of reading materials loaded from a file.
+    - quiz (Quiz): Quiz object loaded from a file.
+    """
+
     def __init__(self, course_type):
+        """
+        Initializes the course based on the course type.
+
+        Parameters:
+        - course_type (str): Type of the course to initialize ('AI', 'InfoSec', or 'PyLearn').
+        """
         self.course_type = course_type
         if course_type == "AI":
             self.filepath = "./learning_materials/ai"
@@ -16,15 +32,16 @@ class Course:
             self.reading_materials = Course.loadReadingFromFile(f"{self.filepath}/py_content.txt")
             self.quiz = Quiz.loadQuizFromFile(f"{self.filepath}/py_quiz.txt", f"{self.filepath}/quiz_grade.txt")
     
+    @staticmethod
     def loadReadingFromFile(filename: str):
         """
-        Loads quiz from txt file returning a quiz object
+        Loads reading materials from a file.
 
         Parameters:
-        filename (str): Path to quiz file
+        - filename (str): Path to the reading materials file.
 
-        Returns: 
-        Quiz: Quiz object with questions and answers
+        Returns:
+        - list: Lines of text from the file.
         """
         with open(filename, 'r') as file:
             lines = file.readlines()
