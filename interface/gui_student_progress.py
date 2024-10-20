28f6b984227d8c71ac6df9204b5aa717597dd56d
EllieC919 <102844395+EllieC919@users.noreply.github.com>
2024-10-20 12:43:25 +1100

    Update gui_student_progress.py

--- interface/gui_student_progress.py
+++ interface/gui_student_progress.py
@@ -12,7 +12,7 @@ from functools import partial
 from interface.gui_progress import Progress
 
 class StudentProgress(tk.Frame):
-    def __init__(self, master, administrator_menu, administrator_user):
+    def __init__(self, master, menu, user):
         """
         Constructor for the SearchTeachers class.
 
@@ -23,8 +23,8 @@ class StudentProgress(tk.Frame):
         """
         super().__init__(master)
         self.master = master
-        self.administrator_menu = administrator_menu
-        self.administrator_user = administrator_user
+        self.menu = menu
+        self.user = user
         
         # Instrument label widget
         self.view_label = tk.Label(master=self, text="View student information:")
@@ -38,7 +38,7 @@ class StudentProgress(tk.Frame):
         
     def show_student_list(self):
         self.student_buttons = []
-        for index,student in enumerate(self.administrator_user.students):
+        for index,student in enumerate(self.user.students):
             show_edit_student_info_with_student = partial(self.show_chosen_student_info, student)
             new_button = tk.Button(master=self, text=f"{student.first_name} {student.last_name}", command=show_edit_student_info_with_student)
             new_button.grid(row=index+2, column=0, padx=10, pady=10, sticky=tk.E)
@@ -49,41 +49,6 @@ class StudentProgress(tk.Frame):
         view_student_progress.place(relx=.5, rely=.5, anchor=tk.CENTER)
         self.hide_menu()
 
-    def edit_student(self, student_to_edit):
-        """
-        Handles the registration process of a student into the management system.
-        The system should prompt the user for
-            - the student's first name,
-            - the student's last name,
-            - the student's date of birth in DD/MM/YYYY format,
-            - the contact number (of the student or the contact person)
-
-        Parameters:
-        receptionist: an instance of the Receptionist class
-
-        Returns:
-        (None)
-        """
-        if not util.is_valid_first_name(self.firstname_var.get()):
-            self.alert_var.set("Invalid first name!")
-            self.firstname_entry.delete(0,"end")
-        elif not util.is_valid_date_format(self.dob_var.get()):
-            self.alert_var.set("Invalid date of birth!")
-            self.dob_entry.delete(0,"end")
-        elif not util.is_valid_contact_number(self.contact_num_var.get()):
-            self.alert_var.set("Invalid contact number!")
-            self.contact_num_entry.delete(0,"end")
-        elif self.administrator_user.edit_student_data(student_to_edit, self.firstname_var.get(), self.lastname_var.get(), self.dob_var.get(), self.contact_num_var.get(), self.contact_email_var.get(), self.username_var.get(), self.password_var.get()):
-            self.alert_var.set("Successfully registered student")
-            self.firstname_entry.delete(0,"end")
-            self.lastname_entry.delete(0,"end")
-            self.dob_entry.delete(0,"end")
-            self.contact_num_entry.delete(0,"end")
-            self.contact_email_entry.delete(0,"end")
-            self.username_entry.delete(0,"end")
-            self.password_entry.delete(0,"end")
-        else:
-            self.alert_var.set("ERROR")
     
     def return_to_menu(self):
         """
